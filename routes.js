d30bc75b486cc9a9d1f11b5aa6656517c3722d78
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-07-26 16:20:59 -0300

    Validação de envio do formulario no backend

--- routes.js
+++ routes.js
@@ -14,7 +14,17 @@ routes.get('/instructors/create', (req, res) =>{
 })
 
 routes.post('/instructors', (req, res) =>{
-  return res.send('recebido')
+  //req.query = /instructors/:id geralmente usado em requisições GET
+  //req.body = É enviado atravez de um formulário no formato JSON, usado em requisições POST
+  
+  const keys = Object.keys(req.body)
+
+  for (value of keys) {
+    if (req.body[value] == "")
+      return res.send("Please, fill in all fields")
+  }
+
+  return res.send(req.body)
 })
 
 routes.get('/members', (req, res) => {
