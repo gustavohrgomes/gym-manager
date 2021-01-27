9ea8027f3f7291250dd8b4ebee16505773190d0a
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-01-27 00:17:19 -0300

    Create Find & Update Methods on Instructor Model

--- src/app/models/Instructor.js
+++ src/app/models/Instructor.js
@@ -27,9 +27,9 @@ module.exports = {
       INSERT INTO instructors (
         name,
         avatar_url,
+        birth,
         gender,
         services,
-        birth,
         created_at
       ) VALUES ($1, $2, $3, $4, $5, $6)
       RETURNING id
@@ -38,13 +38,63 @@ module.exports = {
     const values = [
       instructor.name,
       instructor.avatar_url,
+      date(instructor.birth).iso,
       instructor.gender,
       instructor.services,
-      date(instructor.birth).iso,
       date(Date.now()).iso,
     ];
 
     db.query(insertIntoInstructors, values, function (err, results) {
+      if (err) return res.send("Database Error!");
+
+      callback(results.rows[0]);
+    });
+  },
+  find(id, callback) {
+    const selectFromInstructors = `
+      SELECT 
+        id,
+        name,
+        avatar_url,
+        gender,
+        services,
+        birth,
+        created_at
+      FROM 
+        instructors
+      WHERE 
+        id = $1
+    `;
+
+    db.query(selectFromInstructors, [id], function (err, results) {
+      if (err) return res.send("Database Error!");
+
+      callback(results.rows[0]);
+    });
+  },
+  update(instructor, callback) {
+    const update = `
+      UPDATE instructors 
+      SET
+        name = ($1),
+        avatar_url = ($2),
+        birth = ($3),
+        gender = ($4),
+        services = ($5)
+      WHERE 
+        id = $6
+    `;
+
+    const values = [
+      instructor.name,
+      instructor.avatar_url,
+      date(instructor.birth).iso,
+      instructor.gender,
+      instructor.services,
+      instructor.id,
+    ];
+
+    db.query(update, values, function (err, results) {
       if (err) return res.send("Database Error!");
 
       callback(results.rows[0]);
